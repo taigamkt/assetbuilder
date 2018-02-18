1522df8ab2441e8345fc94566aad97df45db3676
Mark Hagelberg <mark.hagelberg@gmail.com>
2018-02-17 22:43:49 -0700

    add routing module

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,17 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule, Routes } from '@angular/router';
+import { CreatorFormComponent } from './asset-builder/creator-form/creator-form.component';
+
+const routes: Routes = [
+  { path: 'create', component: CreatorFormComponent }
+];
+
+@NgModule({
+  imports: [
+    CommonModule,
+    RouterModule.forRoot(routes)
+  ],
+  exports: [ RouterModule ]
+})
+export class AppRoutingModule { }
