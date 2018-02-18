de2f1ead53a6a675f797d351d155d2faa99c7c05
Joao Aguiam <joaoaguiam@gmail.com>
2018-02-18 09:25:48 -0700

    CategorizedERC721Token

--- migrations/2_factory_migration.js
+++ migrations/2_factory_migration.js
@@ -1,16 +1,12 @@
 var Migrations = artifacts.require("./Migrations.sol");
 
 var FungibleAssetStoreFactory = artifacts.require("./FungibleAssetStoreFactory.sol");
-var Factory = artifacts.require("./Factory.sol");
-
 
 module.exports = async function(deployer, network, accounts) {
   //console.log(accounts);
   let factoryOwner = accounts[9];
   await Promise.all([
-    deployer.deploy(FungibleAssetStoreFactory, {from: factoryOwner}),
-    deployer.deploy(Factory, {from: factoryOwner})
-
+    deployer.deploy(FungibleAssetStoreFactory, {from: factoryOwner})
   ]);
 
   instances = await Promise.all([
