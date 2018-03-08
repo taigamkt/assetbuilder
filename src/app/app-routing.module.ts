bbf1f14156d38274bdd0afe24fad6766768d93d1
Aleksandar Djordjevic <adjordjevic@firstbeatmedia.com>
2018-03-08 03:30:32 +0100

    Fixing current project components + adding new components foundation + adding SCSS integration + configuring Angular CLI + installing additional dependacies

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,16 @@ import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
 import {RouterModule, Routes} from '@angular/router';
 import {CreatorFormComponent} from './asset-builder/creator-form/creator-form.component';
+import {MarketplaceListingComponent} from './marketplace-listing/marketplace-listing.component';
+import {AssetComponent} from './asset/asset.component';
+import {MyAssetsComponent} from './my-assets/my-assets.component';
 
 const routes: Routes = [
   {path: 'create', component: CreatorFormComponent},
-  {path: '', redirectTo: '/create', pathMatch: 'full'}
+  {path: 'marketplace', component: MarketplaceListingComponent},
+  {path: 'asset', component: AssetComponent},
+  {path: 'my-assets', component: MyAssetsComponent},
+  {path: '**', redirectTo: '', pathMatch: 'full'}
 ];
 
 @NgModule({
